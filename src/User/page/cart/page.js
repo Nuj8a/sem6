d1b9ef83e3bd1639e7ad2950743d58414920fc45
gaurab-hacked <gaurabsunar0001@gmail.com>
2024-01-27 10:56:20 +0545

    ga

--- src/User/page/cart/page.js
+++ src/User/page/cart/page.js
@@ -1,48 +1,54 @@
 "use client";
 import React from "react";
-import Breadcrumb from "../../common/Components/Breadcrumb";
-import { Button, Divider } from "@nextui-org/react";
 import TablePage from "./Table";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import { useNavigate } from "react-router-dom";
+import { IoBag } from "react-icons/io5";
+import Breadcrumb from "../../../Dashboard/common/Navigation/BredCrumb";
 
 const Page = () => {
   const navigate = useNavigate();
   return (
-    <div className="px-2">
-      <Breadcrumb />
-      <div className="grid mt-5 grid-cols-12">
+    <div className="px-2 my-10">
+      <div className="-mb-3 mt-7 text-[var(--text-dark-color)] capitalize font-semibold font-poppins text-3xl">
+        Checkout Websites
+      </div>
+      <div className="flex my-5 justify-between items-center">
+        <Breadcrumb category={"services"} categoryLink={"services"} />
+      </div>
+      <div className="grid mt-5 gap-8 grid-cols-12">
         <div className="col-span-8">
           <TablePage />
         </div>
-        <div className="col-span-4 mx-10 mt-16">
-          <h2 className="text-lg font-semibold text-slate-700 tracking-wide mb-4">
-            CART SUMMARY
-          </h2>
-          <div className="mx-2">
-            <div className="flex w-full justify-between mt-2">
-              <span>Subtotal</span>
-              <span>Rs.150</span>
+        <div className=" col-span-4 font-poppins h-full  ">
+          <div className="px-7 py-5 rounded border border-black/15">
+            <h3 className="text-xl text-[var(--text-dark-color)] font-semibold mb-1">
+              Order Summary
+            </h3>
+            <div className="flex flex-col gap-1 mt-4">
+              <div className="text-sm flex justify-between text-[var(--text-light-color)]">
+                <div>Subtotal:</div>
+                <div>Rs. 1,20,000</div>
+              </div>
+              <div className="text-sm flex justify-between text-[var(--text-light-color)]">
+                <div>Discount:</div>
+                <div>10%</div>
+              </div>
+              <div className="text-sm flex border-dashed justify-between border-y py-3 border-black/15 mt-3 my-2 text-[var(--text-light-color)]">
+                <div className="uppercase">Total:</div>
+                <div className="text-[var(--primary-color)] font-semibold text-base">
+                  Rs. 1,00,000
+                </div>
+              </div>
             </div>
-            <div className="flex w-full justify-between mt-2">
-              <span>Shipping</span>
-              <span>Rs.10</span>
-            </div>
-            <Divider className="my-2" />
-            <div className="flex w-full justify-between mt-2">
-              <span>Total</span>
-              <span>Rs. 160</span>
-            </div>
+
+            <button
+              onClick={() => navigate("/checkout")}
+              className="border w-full py-3 hover:bg-[var(--primary-color)] duration-200 font-semibold font-poppins hover:text-[var(--background-color)] border-[var(--primary-color)] text-[var(--primary-color)] mt-4 rounded-sm capitalize text-sm flex items-center justify-center mb-3 gap-[7px] tracking-wide"
+            >
+              <IoBag className="scale-110 text-base mb-[2px]" />
+              Proceed to checkout ( 1 )
+            </button>
           </div>
-          <Button
-            radius="none"
-            color="primary"
-            className="mt-5 w-full rounded-sm"
-            onClick={() => navigate("/checkout")}
-          >
-            Proceed To Checkout{" "}
-            <ArrowForwardIcon className="text-base -ml-1 mt-[2px]" />
-          </Button>
         </div>
       </div>
     </div>
