61bddb588fcb55b3f058e427cbe2ac33321ca6c2
gaurab-hacked <gaurabsunar0001@gmail.com>
2024-02-22 15:59:41 +0545

    ga

--- src/User/page/Services/Products.js
+++ src/User/page/Services/Products.js
@@ -3,12 +3,25 @@ import CardsProduct from "../../common/Cards/CardsProduct";
 import { Pagination } from "@nextui-org/react";
 import { IoIosArrowDown } from "react-icons/io";
 import { IoSearchSharp } from "react-icons/io5";
+import FindGender from "../../../libs/FindGender";
 
-const Products = ({ heading, data }) => {
+const Products = ({
+  heading,
+  data,
+  setPage,
+  page,
+  maxPage,
+  searchSrt,
+  setSearchStr,
+  genderDrop,
+  setGenderDrop,
+}) => {
   return (
     <div>
       <div className="flex items-center justify-between gap-2 font-poppins border-b pb-2 border-black/15 text-black/80">
-        <div className="text-sm capitalize">50 Items found</div>
+        <div className="text-sm capitalize">
+          {data.length} Item{data.length > 1 && "s"} found
+        </div>
         <div className="flex gap-2">
           <div className="w-[250px] flex items-center h-[35px] border  border-black/20 rounded-sm relative">
             <input
@@ -16,27 +29,46 @@ const Products = ({ heading, data }) => {
               name="search"
               id="search"
               className="outline-none absolute inset-0 pr-7 text-sm px-3 bg-transparent h-full w-full"
-              placeholder="Search..."
+              value={searchSrt}
+              onChange={(e) => setSearchStr(e.target.value)}
+              placeholder="Search with name..."
             />
             <IoSearchSharp className="absolute right-1 text-xl text-black/80 hover:text-[var(--text-dark-color)] cursor-pointer" />
           </div>
           <div className="w-[150px] group relative pb-1 text-xs  rounded-sm">
-            <div className="h-[35px] flex justify-between px-3 cursor-pointer border  border-black/20 rounded-sm items-center w-full">
-              Latest Items <IoIosArrowDown />
+            <div className="h-[35px] flex justify-between px-3 cursor-pointer border font-poppins border-gray-500 rounded-sm items-center w-full">
+              {Number(genderDrop) === -1
+                ? "Filter Gender"
+                : Number(genderDrop) === 0
+                  ? "All"
+                  : FindGender(Number(genderDrop))}
+              <IoIosArrowDown />
             </div>
             <div className="absolute z-50 top-[37px] bg-slate-50 border border-black/15 rounded-sm hidden group-hover:block w-full right-0">
               <ul>
-                <li className="p-2 px-3 cursor-pointer hover:bg-blue-200">
-                  Apple
+                <li
+                  className="p-2 px-3 cursor-pointer hover:bg-blue-200"
+                  onClick={() => setGenderDrop(0)}
+                >
+                  All
                 </li>
-                <li className="p-2 px-3 cursor-pointer hover:bg-blue-200">
-                  Ball
+                <li
+                  className="p-2 px-3 cursor-pointer hover:bg-blue-200"
+                  onClick={() => setGenderDrop(1)}
+                >
+                  Men
                 </li>
-                <li className="p-2 px-3 cursor-pointer hover:bg-blue-200">
-                  Cat
+                <li
+                  className="p-2 px-3 cursor-pointer hover:bg-blue-200"
+                  onClick={() => setGenderDrop(2)}
+                >
+                  Women
                 </li>
-                <li className="p-2 px-3 cursor-pointer hover:bg-blue-200">
-                  Dog
+                <li
+                  className="p-2 px-3 cursor-pointer hover:bg-blue-200"
+                  onClick={() => setGenderDrop(3)}
+                >
+                  Unisex
                 </li>
               </ul>
             </div>
@@ -48,9 +80,17 @@ const Products = ({ heading, data }) => {
           return <CardsProduct key={e._id} data={e} />;
         })}
       </div>
-      <div className="mt-10 float-right" id="pagination">
-        <Pagination isCompact showControls total={10} initialPage={1} />
-      </div>
+      {maxPage > 1 && (
+        <div className="mt-10 float-right" id="pagination">
+          <Pagination
+            isCompact
+            showControls
+            total={maxPage}
+            initialPage={page}
+            onChange={setPage}
+          />
+        </div>
+      )}
     </div>
   );
 };
--- src/User/page/Services/ServiceLayout.js
+++ src/User/page/Services/ServiceLayout.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Filter from "./Filter";
 import Products from "./Products";
 import Services from "../../common/page/Services";
 import Related from "../../common/page/Related";
 
 const ServiceLayout = ({ data, filterData, setFilterData }) => {
+  const [page, setPage] = useState(1);
+
+  // =================== for filter (start)================
+  const [searchSrt, setSearchStr] = useState("");
+  const [genderDrop, setGenderDrop] = useState(-1);
+
+  const afterSearch =
+    searchSrt.length > 2
+      ? data.filter((e) =>
+          e.title.toLowerCase().includes(searchSrt.toLowerCase())
+        )
+      : data;
+
+  const lastSearch =
+    Number(genderDrop) !== 0 && Number(genderDrop) !== -1
+      ? afterSearch.filter((e) => Number(e.gendertype) === Number(genderDrop))
+      : afterSearch;
+
+  // =================== for filter (end)================
+
+  const itemsPerPage = 15;
+  const maxPage = Math.ceil(lastSearch.length / itemsPerPage);
+
+  const startIndex = (page - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const slicedData = lastSearch.slice(startIndex, endIndex);
+
   return (
     <div className="grid gap-7 px-2 grid-cols-11">
       <div className="w-full flex flex-col col-span-3 ">
@@ -13,7 +40,16 @@ const ServiceLayout = ({ data, filterData, setFilterData }) => {
         </div>
       </div>
       <div className="w-full col-span-8">
-        <Products data={data} />
+        <Products
+          data={slicedData}
+          setPage={setPage}
+          page={page}
+          maxPage={maxPage}
+          searchSrt={searchSrt}
+          setSearchStr={setSearchStr}
+          genderDrop={genderDrop}
+          setGenderDrop={setGenderDrop}
+        />
       </div>
       <div className="col-span-11">
         <div>
